aaf72e984b2c5f4251aee367c87aec075bcb32cc
NicolasLacerda <nicolas.lacerda.work@gmail.com>
2022-11-12 11:06:00 -0300

    .

---
+++ Att1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int inteiro;
+    inteiro = 5;
+    cout << inteiro <<endl;
+
+    float numeroComVirgula;
+    numeroComVirgula = 5.4;
+    cout << numeroComVirgula <<endl;
+    
+    float numeroElevado;
+    numeroElevado = 5e37;
+    cout << numeroElevado <<endl;
+
+    double numeroElevadoGrande;
+    numeroElevadoGrande = 5e307;
+    cout << numeroElevadoGrande <<endl;
+
+    bool booleano;
+    booleano = true;
+    cout << booleano <<endl;
+
+    char caractere;
+    caractere = 'e';
+    cout << caractere <<endl;
+
+    string palavras;
+    palavras = "teste";
+    cout << palavras <<endl;
+
+    return 0;
+}
